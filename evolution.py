f05474c243e9f92aeb562ab31283680107031b57
Matthias P. Nowak <matth-no@online.no>
2018-05-30 10:48:16 +0200

    Redone imports, generate picture labels in program

--- evolution.py
+++ evolution.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python
 import sys
 
-from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, Qt, QtWidgets, QtGui
 
 from MainWindow import Ui_MainWindow
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        ag=QActionGroup(self)
+        self.pics=[]
+        for i in range(3):
+            for j in range(3):
+                ql= QtWidgets.QLabel(self.centralwidget)
+                self.pics.append(ql)
+                self.gl.addWidget(ql,i,j,1,1)
+                ql.setText( 'ah %d %d' % (i,j))
+        ag=QtWidgets.QActionGroup(self)
         self.actionBreed.setActionGroup(ag)
         self.actionKill.setActionGroup(ag)
         #
         self.actionExit.triggered.connect(self.appExit)
         self.actionFullScreen.triggered.connect(self.fullScreen)
+        #
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
        # last one
         self.show()
 
@@ -32,10 +41,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def fullScreen(self):
         self.showFullScreen()
 
+    def keyPressEvent(self,e):
+        print('key pressed')
 
 if __name__ == '__main__':
     print('running evolution')
-    app=QApplication([])
+    app=Qt.QApplication([])
     app.setApplicationName('Evolution')
     window = MainWindow()
     app.exec_()
